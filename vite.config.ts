f8660df26695c5c33ca1f9227ca20ae594e249e4
sandrinopatrulescu <sandryno_sandryno@yahoo.com>
2025-08-21 20:10:13 +0300

    install nuxt/ui
    https://ui.nuxt.com/getting-started/installation/vue
    
    Took 13 minutes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import ui from '@nuxt/ui/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +11,7 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    ui(),
   ],
   resolve: {
     alias: {
